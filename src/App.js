7cf1c7a072cd9dbd56f2cf88b9c2a313250a52e2
Andrew Fan <andrewfan030114@gmail.com>
2023-06-22 23:04:28 -0700

    Updates list of exercises dynamically

--- src/App.js
+++ src/App.js
@@ -2,37 +2,46 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Exercise from './components/Exercise';
-import NewExercise from './components/NewExercise';
+import AddExercise from './components/AddExercise';
+import { useState } from 'react';
 
-
+const DUMMYexercises = [
+  {
+    movement: 'Low Bar Paused Squat',
+    scheme: '1 set of 4',
+    rpe: 7,
+    weight: '215 kg',
+  },
+  {
+    movement: 'Low Bar Comp Squat',
+    scheme: '2 sets of 6',
+    rpe: 6,
+    weight: '175 kg',
+  },
+  {
+    movement: 'Tempo Paused Bench',
+    scheme: '3 sets of 7',
+    rpe: 7,
+    weight: '125 kg',
+  },
+  {
+    movement: 'Tempo Paused Bench ggggggggggggggggg',
+    scheme: '3 sets of 7',
+    rpe: 7,
+    weight: '125 kg',
+  },
+];
 function App() {
-  const exercises = [
-    {
-      movement: 'Low Bar Paused Squat',
-      scheme: '1 set of 4',
-      rpe: 7,
-      weight: '215 kg',
-    },
-    {
-      movement: 'Low Bar Comp Squat',
-      scheme: '2 sets of 6',
-      rpe: 6,
-      weight: '175 kg',
-    },
-    {
-      movement: 'Tempo Paused Bench',
-      scheme: '3 sets of 7',
-      rpe: 7,
-      weight: '125 kg',
-    },
-    {
-      movement: 'Tempo Paused Bench ggggggggggggggggg',
-      scheme: '3 sets of 7',
-      rpe: 7,
-      weight: '125 kg',
-    },
-  ];
   const WeekNumber = '1';
+
+  const [exercises, setExercises] = useState(DUMMYexercises);
+
+  const addExerciseHandler = (exercise) => {
+    setExercises((prevExercises) => {
+      return [...prevExercises, exercise];
+    });
+  };
+
   return (
     <div className="App">
       <div className="App-header">
@@ -40,7 +49,7 @@ function App() {
       </div>
       <div className="App-exercise">
         <Exercise props={exercises} />
-        <NewExercise />
+        <AddExercise onAddNewExercise={addExerciseHandler} />
       </div>
     </div>
   );
