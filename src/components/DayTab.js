79bc1023078a2d181dbbfbba0fddacca3e39300e
Andrew Fan <andrewfan030114@gmail.com>
2023-06-25 16:11:43 -0700

    Updated days of the week and added rest day msg

--- src/components/DayTab.js
+++ src/components/DayTab.js
@@ -2,31 +2,11 @@ import React, { useContext, useState } from 'react';
 import './DayTab.css';
 import Exercise from './Exercise';
 import { TabContext } from './Tabs/TabProvider';
+import image from '../assets/corgi.png';
 
 const DayTab = ({ props }) => {
   const { activeTab, switchTab } = useContext(TabContext);
-  const tabContent = [
-    <div>
-      <Exercise
-        props={props.filter((exercise) => exercise.day === activeTab)}
-      />
-    </div>,
-    <div>
-      <Exercise
-        props={props.filter((exercise) => exercise.day === activeTab)}
-      />
-    </div>,
-    <div>
-      <Exercise
-        props={props.filter((exercise) => exercise.day === activeTab)}
-      />
-    </div>,
-    <div>
-      <Exercise
-        props={props.filter((exercise) => exercise.day === activeTab)}
-      />
-    </div>,
-  ];
+  const dayNames = ['Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat', 'Sun'];
 
   const tabClickHandler = (tabIndex) => {
     switchTab(tabIndex);
@@ -35,17 +15,28 @@ const DayTab = ({ props }) => {
   return (
     <div className="Overall">
       <div className="Tabs">
-        {tabContent.map((cotent, index) => (
+        {dayNames.map((day, index) => (
           <button
             key={index}
             className={index === activeTab ? 'active' : ''}
             onClick={() => tabClickHandler(index)}
           >
-            Day {index + 1}
+            {day}
           </button>
         ))}
       </div>
-      <div className="tab-content">{tabContent[activeTab]}</div>
+      <div className="Tab-content">
+        {props.filter((exercise) => exercise.day === activeTab).length === 0 ? (
+          <div>
+            <img src={image} className="image" />
+            <p>Rest day!</p>
+          </div>
+        ) : (
+          <Exercise
+            props={props.filter((exercise) => exercise.day === activeTab)}
+          />
+        )}
+      </div>
     </div>
   );
 };
