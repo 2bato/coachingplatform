a94b7004d265fefcc38c1fa95b4d847f843249d8
Andrew Fan <andrewfan030114@gmail.com>
2023-06-25 15:14:36 -0700

    Added Day filter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { TabProvider } from './components/Context/TabProvider';
+import { TabProvider } from './components/Tabs/TabProvider';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
