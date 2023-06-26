0e261443757fb996d0c7649888847c601f040304
Andrew Fan <andrewfan030114@gmail.com>
2023-06-25 17:11:00 -0700

    Styling updates and directory organization

--- src/components/Exercises/WeightVisualizer.js
+++ src/components/Exercises/WeightVisualizer.js
@@ -1,10 +1,10 @@
 import './WeightVisualizer.css';
 
-const Plate = ({ color, height }) => (
+const Plate = ({ color, width, height }) => (
   <div
     style={{
       backgroundColor: color,
-      width: `0.4vw`,
+      width: `${width}vw`,
       height: `${height}vh`,
       border: '0.1px solid black',
       borderRadius: '1.5px',
@@ -14,13 +14,13 @@ const Plate = ({ color, height }) => (
 
 export default function WeightVisualizer({ weight }) {
   const plates = [
-    { value: 25, color: 'red', height: 5},
-    { value: 20, color: 'blue', height: 5},
-    { value: 15, color: 'yellow', height: 4.5},
-    { value: 10, color: 'green', height: 3.8},
-    { value: 5, color: 'white',  height: 3},
-    { value: 2.5, color: '#2d2d2d', height: 2.5},
-    { value: 1.25, color: 'silver', height: 2 },
+    { value: 25, color: 'red', width: 1.4, height: 5 },
+    { value: 20, color: 'blue', width: 1.4, height: 5 },
+    { value: 15, color: 'yellow', width: 1.2, height: 4.5 },
+    { value: 10, color: 'green', width: 1, height: 3.8 },
+    { value: 5, color: 'white', width: 1, height: 3 },
+    { value: 2.5, color: '#2d2d2d', width: 0.8, height: 2.5 },
+    { value: 1.25, color: 'silver', width: 0.7, height: 2 },
   ];
 
   const renderPlates = () => {
@@ -31,7 +31,7 @@ export default function WeightVisualizer({ weight }) {
       const { value, ...rectProps } = plate;
 
       while (remainingWeight >= value) {
-        renderedPlates.push(<Plate key = {crypto.randomUUID()} {...rectProps} />);
+        renderedPlates.push(<Plate key={crypto.randomUUID()} {...rectProps} />);
         remainingWeight -= value;
       }
     }
